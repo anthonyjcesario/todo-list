ad1c08336e8e78d3e015e5de8ac3101dc5d49c42
Anthony Cesario <ajcesario4746@gmail.com>
2025-03-11 17:35:56 -0600

    Added functionality that tells the user if they try to use a username that is already in use.

--- Controllers/Register.php
+++ Controllers/Register.php
@@ -2,9 +2,21 @@
 
 class Register extends Controller {
     public static function setUser($usr, $pwd) {
-        $sql = "INSERT INTO users (username, pwd) VALUES (?, ?)";
+        #First, I want to check if this username has already been used:
+        $sql = "SELECT * FROM users WHERE username = ?";
         $stmt = self::connect()->prepare($sql);
-        $stmt->execute([$usr, $pwd]);
-        echo("<h5 class='m-4'>User account created!</h5>");
+        $stmt->execute(array($usr));
+        $rowNum = $stmt->fetchColumn();
+
+        if ($rowNum == 0) {
+            $sql = "INSERT INTO users (username, pwd) VALUES (?, ?)";
+            $stmt = self::connect()->prepare($sql);
+            $stmt->execute([$usr, $pwd]);
+            echo("<h5 class='m-4'>User account created!</h5>");
+        } else {
+            echo("<h5 class='m-4'>This username is already in use!</h5>");
+        }
+
+        
     }
 }
