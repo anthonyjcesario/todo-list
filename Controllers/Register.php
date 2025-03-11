6c543095523e771b54cae20b703162cb8395669e
Anthony Cesario <ajcesario4746@gmail.com>
2025-03-11 17:31:52 -0600

    Added a message once a user registers to let them know that registration was successful.

--- Controllers/Register.php
+++ Controllers/Register.php
@@ -5,5 +5,6 @@ class Register extends Controller {
         $sql = "INSERT INTO users (username, pwd) VALUES (?, ?)";
         $stmt = self::connect()->prepare($sql);
         $stmt->execute([$usr, $pwd]);
+        echo("<h5 class='m-4'>User account created!</h5>");
     }
 }
